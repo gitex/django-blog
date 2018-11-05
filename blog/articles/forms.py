dca909966cb24e69ffd18417f4adb37fd2f69dd4
Evgeniy <evgeniy.dorovatovskiy@kivano.ru>
2018-11-05 23:46:47 +0600

    Set Floara to create post

--- blog/articles/forms.py
+++ blog/articles/forms.py
@@ -5,13 +5,14 @@ from django import forms
 
 from froala_editor.widgets import FroalaEditor
 from django_select2.forms import Select2MultipleWidget, Select2Widget
-from taggit.models import Tag, TaggedItem
+from taggit.models import Tag
 
 from .models import Post, Category
 
 
 class PostForm(forms.ModelForm):
     body = forms.CharField(widget=FroalaEditor)
+
     category = forms.ModelChoiceField(
         queryset=Category.objects.all(),
         widget=Select2Widget
